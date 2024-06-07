74de587dba8e79e7db40ae4a7ad3767f7847a921
vladislav-chihun <vladikkosmo@gmail.com>
2024-06-07 10:36:28 +0200

    create persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactReducer from "./contactsSlice";
 import filterReducer from "./filtersSlice";
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
@@ -15,7 +20,13 @@ export const store = configureStore({
     reducer: {
         contacts: persistedContactReducer,
         filters: filterReducer,
-    },
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store)
